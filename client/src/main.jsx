8017b9d9bda80e75af16f755b7990d066c440186
Jens <jens@syntetika.io>
2026-05-07 21:03:54 +0200

    refactor: split globals.css into per-feature stylesheets

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,8 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import { initTelegram } from './lib/telegram.js';
-import './styles/tokens.css';
-import './styles/globals.css';
+import './styles/index.css';
 
 initTelegram();
 
